4feb14d250bbe9147f02aee5b80642037b4310f9
LastExceed <chris20194@googlemail.com>
2026-08-27 02:09:20 +0200

    emit `#[automatically_derived]`

--- azo-sys/src/utils.rs
+++ azo-sys/src/utils.rs
@@ -18,6 +18,7 @@ macro_rules! c_enum {
 			)+
 		}
 		
+		#[automatically_derived]
 		impl std::fmt::Display for $enum_name {
 			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 				let name = match *self {
@@ -31,18 +32,21 @@ macro_rules! c_enum {
 			}
 		}
 		
+		#[automatically_derived]
 		impl std::fmt::Debug for $enum_name {
 			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 				write!(f, "{}::{}({})", stringify!($enum_name), self, self.0)
 			}
 		}
 		
+		#[automatically_derived]
 		impl From<std::ffi::c_long> for $enum_name {
 			fn from(value: std::ffi::c_long) -> Self {
 				Self(value)
 			}
 		}
 		
+		#[automatically_derived]
 		impl From<$enum_name> for std::ffi::c_long {
 			fn from(value: $enum_name) -> Self {
 				value.0
